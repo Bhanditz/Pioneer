3ce6c785806534af41c8f1a87189390802089174
Ives van Hoorne <Ives.v.h@gmail.com>
2015-10-10 18:42:17 +0200

    Base project in Scala

--- build.sbt
+++
@@ -1,7 +0,0 @@
-name := "libgdx-sbt"
-
-version := "0.1.0"
-
-organization := "com.hagerbot"
-
-seq(giter8Settings :_*)
---
+++ desktop/src/com/bydrives/pioneer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bydrives.pioneer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bydrives.pioneer.Pioneer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Pioneer(), config);
+	}
+}
