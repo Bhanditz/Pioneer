4f0fe335247fec880dceda11316afd7b781ca523
Ives van Hoorne <Ives.v.h@gmail.com>
2016-01-09 23:40:19 +0100

    Add z to rendering

--- core/src/com/bydrives/pioneer/systems/client/render/TileRenderSystem.scala
+++ core/src/com/bydrives/pioneer/systems/client/render/TileRenderSystem.scala
@@ -5,9 +5,8 @@ import com.artemis.systems.IteratingSystem
 import com.artemis.{Aspect, ComponentMapper}
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.bydrives.pioneer.components.{TileType, Position}
 import com.bydrives.pioneer.components.client.Visual
-import com.bydrives.pioneer.world.Tile
+import com.bydrives.pioneer.components.{Position, TileType}
 
 /**
  * Created by ivesv on 10/10/2015.
@@ -21,6 +20,6 @@ class TileRenderSystem(batch: SpriteBatch, camera: OrthographicCamera) extends I
   override def process(e: Int): Unit = {
     val visual: Visual = cVisual.get(e)
     val position: Position = cPosition.get(e)
-    batch.draw(visual.texture, position.x, position.y, visual.width, visual.height)
+    batch.draw(visual.texture, position.x, position.y + position.z, visual.width, visual.height)
   }
 }
