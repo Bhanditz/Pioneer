ddd77d4cd8a72597db3d6513e75f2ea5c34b35b8
Ives van Hoorne <Ives.v.h@gmail.com>
2016-01-09 22:02:42 +0100

    Removed dependencies

--- core/src/com/bydrives/pioneer/factories/TileFactory.scala
+++ core/src/com/bydrives/pioneer/factories/TileFactory.scala
@@ -3,7 +3,6 @@ package com.bydrives.pioneer.factories
 import com.artemis._
 import com.bydrives.pioneer.components.client.Visual
 import com.bydrives.pioneer.components.{Position, TileType}
-import com.bydrives.pioneer.utils.SimplexNoise
 import com.bydrives.pioneer.world.Tile
 
 /**
@@ -24,59 +23,6 @@ object TileFactory {
     visual.width = 1
     visual.height = 1
 
-    createDecals(tile, x, y, world)
     entity
-  }
-
-  def createDecals(tile: Tile, x: Int, y: Int, world: World): Unit = {
-    if(tile.decals.isEmpty) return
-
-    val position: ComponentMapper[Position] = world.getMapper(classOf[Position])
-    val visual: ComponentMapper[Visual] = world.getMapper(classOf[Visual])
-
-    val density = tile.decalDensity.toFloat
-    // Assuming that all tiles are square
-    val tileSize = tile.texture.getHeight.toFloat
-    var stepSize = density/tileSize
-    var xStep = x.toFloat
-    var yStep = y.toFloat
-
-
-    val persistence: Float = 1
-
-
-    while (xStep <= x + 1) {
-      yStep = y.toFloat
-      while (yStep <= y + 1) {
-        var amplitude: Float = 1
-        var frequency: Float = 1
-        var maxValue: Float = 1
-        val noise = (0 until 6 map((i: Int) => {
-          val lNoise = SimplexNoise.noise(xStep, yStep) * amplitude
-
-          maxValue += amplitude
-
-          amplitude *= persistence
-          frequency *= 2
-          lNoise
-        }) sum) / maxValue
-        if(noise.abs > 0.6) {
-          val decal = tile.decalQuantityList((Math.random() * tile.decalQuantityList.length.toFloat).toInt)
-          val entity: Entity = world.createEntity()
-          val positionComponent = position.create(entity)
-          val visualComponent = visual.create(entity)
-          positionComponent.x = xStep
-          positionComponent.y = yStep
-          visualComponent.texture = decal.texture
-          visualComponent.width = decal.width
-          visualComponent.height = decal.height
-
-          xStep += visualComponent.width / 2
-          yStep += visualComponent.height / 2
-        }
-        yStep += stepSize
-      }
-      xStep += stepSize
-    }
   }
 }
